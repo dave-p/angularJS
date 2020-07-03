718473f872bb480ca0b1728b2614c0ccb1379b43
Poul Kalff <poulkalff@gmail.com>
2020-07-03 17:10:12 +0200

    Dvd-page now has basic functionality

--- scripts/dvrController.js
+++ scripts/dvrController.js
@@ -2,20 +2,41 @@
 app.controller('dvrController', function($scope, $http) {
 
 
+	// used by main table, for sorting
+	$scope.sort = {
+			column: 'status',
+			descending: false
+		};
+		
+	// called by table headers, changes sorting
+	$scope.changeSorting = function(column) {
+
+		var sort = $scope.sort;
+
+		if (sort.column == column) {
+			sort.descending = !sort.descending;
+		} else {
+			sort.column = column;
+			sort.descending = false;
+		}
+	};
+
+	// check all checkboxes
+    $scope.checkAll = function () {
+		var checkBoxes = document.getElementsByClassName("dvrCheckbox");
+        angular.forEach(checkBoxes, function (item) {
+            item.checked = !$scope.selectAll;
+        });
+    };
+
 
 	// MAIN: Program starts here
 	$http.get("/api/dvr/entry/grid")
-		.then(function (reply)
-		{
-			var dvrMain = document.getElementById('dvr_main');
-			angular.forEach(reply['data']['entries'], function (value)
-			{
-				dvrMain['innerText'] += (value['disp_title'] + '\n');
-			})
-		})
+		.then(function (response) {	$scope.dvrData = response.data.entries });
 
 
 	}); // controller ends
+
 
 
 
